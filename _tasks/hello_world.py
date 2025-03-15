266ae4f6175541b72d45c511d398c24198d9b1f0
Jian Huang <jianhuanggo@gmail.com>
2025-03-15 15:03:17 -0700

    initial

---
+++ _tasks/hello_world.py
@@ -0,0 +1,7 @@
+def greet_user(username):
+    """
+    Function to print hello world message with username
+    Args:
+        username (str): Name of the user to greet
+    """
+    return f"Hello World, {username}!"
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def greet_user(username):
     Args:
         username (str): Name of the user to greet
     """
-    print(f"Hello World, {username}!")
+    from _tasks.hello_world import greet_user
+    return greet_user(username)  # Call the imported function to get the greeting message
+
 
 # Example usage
 if __name__ == "__main__":
