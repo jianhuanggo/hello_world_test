f8a358ab49acf4f13979c91fa47bdeb68cfef399
Jian Huang <jianhuanggo@gmail.com>
2025-03-15 14:38:12 -0700

    initial

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+def greet_user(username):
+    """
+    Function to print hello world message with username
+    Args:
+        username (str): Name of the user to greet
+    """
+    print(f"Hello World, {username}!")
+
+# Example usage
+if __name__ == "__main__":
+    # Test the function
+    greet_user("John") 
